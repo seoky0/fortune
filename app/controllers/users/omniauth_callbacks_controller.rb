1a8d5e128e203cdbf2a3e936e869757a87cdc39b
kyohei0423 <kyo199333@gmail.com>
2017-02-05 13:09:07 +0900

    リファクタリング

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to root_path
-    end
+    callback_from(:facebook)
   end
 
   def passthru
@@ -22,5 +15,19 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def after_omniauth_failure_path_for(scope)
     super(scope)
+  end
+
+  private
+
+  def callback_from(provider)
+    provider = provider.to_s
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to root_path
+    end
   end
 end
